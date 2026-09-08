58c6663d5b0dcb7dcc018ca209c3340b79676465
TAGUCHI-MAKOTO <akuteon84@gmail.com>
2026-09-09 03:36:02 +0900

    Delete メンション依頼フォーム_V17_複数PCテスト directory

--- メンション依頼フォーム_V17_複数PCテスト/make_session_id.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    function p2(n){ return n < 10 ? "0" + n : "" + n; }
-    var d = new Date();
-    var s = "V17_" + d.getFullYear() + p2(d.getMonth()+1) + p2(d.getDate()) + "_" +
-            p2(d.getHours()) + p2(d.getMinutes()) + p2(d.getSeconds());
-    WScript.Echo(s);
-})();
